41cce879028b7cfa517682594a7107062653c0d2
robertSimionescu23 <simionescurobert23@gmail.com>
2025-07-13 22:26:03 +0300

    first commit

---
+++ src/main/java/dev/robert/spring_boot/animal_shelter_spring/AnimalShelterSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.robert.spring_boot.animal_shelter_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalShelterSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/robert/spring_boot/animal_shelter_spring/AnimalShelterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.robert.spring_boot.animal_shelter_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
