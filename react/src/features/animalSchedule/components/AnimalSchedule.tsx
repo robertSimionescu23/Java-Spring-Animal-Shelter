1260de16a0e82d40710a299f26a2f10e6edf78b0
robertSimionescu23 <simionescurobert23@gmail.com>
2025-10-27 23:20:24 +0200

    Managed to create visual visit blocks on the schedule.

--- react/src/features/animalSchedule/components/AnimalSchedule.tsx
+++ react/src/features/animalSchedule/components/AnimalSchedule.tsx
@@ -1,7 +1,6 @@
-import { data, useParams } from "react-router-dom";
 import styles from "./animalSchedule.module.css"
 import saschaImg from "../assets/sasha.jpg"
-import { startTransition, useState } from "react";
+import { useEffect, useState } from "react";
 
 function AnimalSchedule(){
 
@@ -46,81 +45,110 @@ function AnimalSchedule(){
         sundaySchedule,
     ];
 
-    const [scheduleGrid, setScheduleGrid] = useState<boolean[][]>(masterSchedule);
 
-    const [choosingHours, setChoosingHours] = useState<boolean>(true);
+    const [scheduleGrid] = useState<boolean[][]>(masterSchedule);
 
-    type time = {
+    type TimeIndexed = {
         hour: number,
         day: number
     };
 
-    const [startingTime, setStartingTime] = useState<time | null>(null);
-    const [endingTime, setEndingTime] = useState<time | null>(null);
 
-    const toggleCell = (dayIndex: number, hourIndex: number):void => {
-        //TODO: Rework
-    };
+    const [endingTime, setEndingTime]    = useState<TimeIndexed| null>(null);
+    const [startingTime, setStartingTime] = useState<TimeIndexed| null>(null);
+    const [visitMap, setVisitMap] = useState<Map<string, number>>(new Map());
 
-    const getSpan = ():number =>{
-        if(startingTime && endingTime){
-            if(startingTime.hour > endingTime.hour)
-                return startingTime.hour - endingTime.hour + 1;
-            else if(startingTime.hour < endingTime.hour)
-                return endingTime.hour - startingTime.hour + 1;
-            else return 1;
+    useEffect(() => {
+        if(endingTime && startingTime){
+            const tempMap = new Map(visitMap);
+            if(startingTime.hour < endingTime.hour)
+                tempMap.set(`${startingTime.day}-${startingTime.hour}`, Math.abs(startingTime.hour - endingTime.hour) + 1);
+            else
+                tempMap.set(`${endingTime.day}-${endingTime.hour}`, Math.abs(startingTime.hour - endingTime.hour) + 1);
+            setVisitMap(tempMap);
         }
-        else
-            return 1;
-    }
+        setStartingTime(null);
+        setEndingTime(null);
+    }, [endingTime]);
 
-    const getStartingPoint = (): number =>{
-        if(startingTime && endingTime){
-            if(startingTime.hour > endingTime.hour)
-                return endingTime.hour;
-            else if(startingTime.hour < endingTime.hour)
-                return startingTime.hour;
-            else return startingTime.hour;
+    useEffect(()=>{
+        console.log(visitMap);
+    },[visitMap])
+
+
+    const handleClick = (dayIndex: number, hourIndex:number) => {
+        if (startingTime == null)
+            setStartingTime({day:dayIndex, hour:hourIndex});
+        else{
+            //Case where another column has been clicked (days do not match)
+            if(startingTime.day != dayIndex){
+                setStartingTime(null);
+                setEndingTime(null);
+            }
+            else{
+                let isFree: boolean = true;
+                let startingPoint: number = startingTime.hour;
+                let endingPoint: number;
+
+                //Switch starting point of visit depending on the clicked time slots
+                if(startingTime.hour < hourIndex)
+                    endingPoint = hourIndex;
+                else{
+                    endingPoint = startingTime.hour;
+                    startingPoint = hourIndex;
+                }
+
+                //Make sure that no 2 visits overlap
+                for(let i: number = startingPoint; i <= endingPoint; i ++)
+                    if(scheduleGrid[dayIndex][i] ==  true)
+                        isFree = false;
+
+                if(isFree){
+                    for(let i: number = startingPoint; i <= endingPoint; i ++)
+                        scheduleGrid[dayIndex][i] = true;
+                    setEndingTime({day:dayIndex, hour:hourIndex});
+                }
+                //If they overlap, start over
+                else{
+                    setStartingTime(null);
+                }
+
+            }
         }
-        else
-            return 1;
-    }
+        console.log(startingTime);
+        //TODO: Make it so that the first time slot pressed is highlighted
 
-    const chooseCell= (dayIndex: number, hourIndex: number):boolean =>{
-        if(!startingTime || !endingTime)
-            return false;
-        if(dayIndex != startingTime.day)
-            return false
-        if(hourIndex != getStartingPoint())
-            return false;
-        return true;
     }
-
 
 
     return(
         <section className = {styles.mainWrapper}>
             <div className = {styles.scheduleControl}>
                 <div className = {`${styles.interSection} ${styles.gridElement}`}></div>
-                {hours.map((hour, index) =>(
+                {hours.map((hour, index) =>(                 //Hour Column on left
                     <div key = {index} className = {`${styles.hourGrid} ${styles.gridElement}`}>{hour}</div>
                 ))}
-                {days.map((day, index) =>(
+                {days.map((day, index) =>(                   //Day Column on top
                     <div key = {index} className = {`${styles.dayGrid} ${styles.gridElement}`}>{day}</div>
                 ))}
                 <div className = {`${styles.interSection} ${styles.gridElement}`}></div>
+                {/* Day index are from 0 to 7. Hour Indexes follow teh same principle */}
                 {scheduleGrid.map((day, dayIndex)=>(
                     day.map((hour, hourIndex)=>(
                          <div key={`${dayIndex}-${hourIndex}`}
-                            className={hour?`${styles.gridElement} ${styles.scheduleBlock}` : `${styles.gridElement} ${styles.scheduleBlockNoAfter}`}
-                            style={{"gridRow":`${hourIndex + 2} / span ${chooseCell(dayIndex, hourIndex)?getSpan(): 1}`,"gridColumn":`${dayIndex + 2}`,
-                                   "borderBottom":`${(hourIndex%4 == 3)? "1px solid black":""}`
+                            className={hour?`${styles.gridElement} ${styles.scheduleBlock}`:`${styles.gridElement} ${styles.scheduleBlockNoAfter}`}
+                            style={{"gridRow":`${hourIndex + 2} / span ${visitMap.has(`${dayIndex}-${hourIndex}`)?visitMap.get(`${dayIndex}-${hourIndex}`) : 1}`,
+                                   "gridColumn":`${dayIndex + 2}`,
+                                   "borderBottom":`${(hourIndex%4 == 3)? "1px solid black":""}`,
+                                   "zIndex":`${visitMap.has(`${dayIndex}-${hourIndex}`)?"2" : "1"}`
                                    }}
-                            onClick={( ) => toggleCell(dayIndex, hourIndex)}
+                            onClick={() => handleClick(dayIndex, hourIndex)}
                         >
+                            {visitMap.has(`${dayIndex}-${hourIndex}`)?`Visit scheduled`:""}
                         </div>
                     ))
                 ))}
+
             </div>
             {/* TODO:Make this be the photo from the backend */}
             {/* TODO:Make this a full picture show off */}
